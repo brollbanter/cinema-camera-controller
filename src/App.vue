8e54c39804d274d55b22bd34a0da7cd1d879b733
Brian VanLoo <brian@brianmakesthings.com>
2024-04-21 12:41:05 -0700

    Add a back button for all pages except Home

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,8 @@
 <template>
   <div id="app" class="h-screen bg-black text-white">
+    <div v-if="this.$route.name != 'Home'" class="flex justify-end">
+      <router-link to="/" class="py-1 px-4 bg-green-500">Back</router-link>
+    </div>
     <router-view/>
   </div>
 </template>
@@ -7,6 +10,9 @@
 <script>
 export default {
   name: 'App',
-}
 
+  created() {
+    console.log(this.$route)
+  },
+}
 </script>
